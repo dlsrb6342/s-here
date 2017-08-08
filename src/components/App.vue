2c97d8cfdd270784cdcb8992905757ba6b9cc5ae
Vaelot <minusgodkeanu@gmail.com>
2017-08-08 17:29:23 +0900

    달력 화살표 추가

--- src/components/App.vue
+++ src/components/App.vue
@@ -59,7 +59,7 @@ export default {
 .fade-enter-active {
   transition-delay: .25s;
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+.fade-enter, .fade-leave-to{
   opacity: 0;
 }
 </style>
--- src/pages/Check/Check.vue
+++ src/pages/Check/Check.vue
@@ -8,9 +8,9 @@
 						<tr>
 							<th colspan="7">
 								<center>
-									<a class="btn"><i class="icon-chevron-left"></i></a>
+									<a class="btn" @click="changeMonth(-1)"><i class="icon-chevron-left"><</i></a>
 									<a class="btn">{{ showFocus }}</a>
-									<a class="btn"><i class="icon-chevron-right"></i></a>
+									<a class="btn" @click="changeMonth(1)"><i class="icon-chevron-right">></i></a>
 								</center>
 							</th>
 						</tr>
@@ -83,6 +83,14 @@ export default {
 				else res.setMonth(this.focus.getMonth() - 1)
 			}
 			res.setDate(day[0])
+			this.focus = res
+			this.drawCalendar
+		},
+		changeMonth (delta) {
+			let res = new Date()
+			res.setDate(this.focus.getDate())
+			res.setFullYear(this.focus.getFullYear())
+			res.setMonth(this.focus.getMonth() + delta)
 			this.focus = res
 			this.drawCalendar
 		},
