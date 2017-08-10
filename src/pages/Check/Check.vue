0e8b4e8f6e273a0254f63a1ee5db1ce6a187ed2e
Vaelot <minusgodkeanu@gmail.com>
2017-08-10 20:08:51 +0900

    Check 시간대 입력기능

--- src/pages/Check/Check.vue
+++ src/pages/Check/Check.vue
@@ -9,19 +9,13 @@
 							<th colspan="7">
 								<center>
 									<a class="btn" @click="changeMonth(-1)"><i class="icon-chevron-left"><</i></a>
-									<a class="btn">{{ showFocus }}</a>
+									<a class="btn" @click="changeToday()">{{ showFocus }}</a>
 									<a class="btn" @click="changeMonth(1)"><i class="icon-chevron-right">></i></a>
 								</center>
 							</th>
 						</tr>
 						<tr>
-							<th><center>일</center></th>
-							<th><center>월</center></th>
-							<th><center>화</center></th>
-							<th><center>수</center></th>
-							<th><center>목</center></th>
-							<th><center>금</center></th>
-							<th><center>토</center></th>
+							<th v-for="(t, index) in ['일', '월', '화', '수', '목', '금', '토']" :key="index"><center>{{ t }}</center></th>
 						</tr>
 					</thead>
 					<tbody>
@@ -33,25 +27,41 @@
 						</tr>
 					</tbody>
 				</table>
-				<a class="btn" @click="showTimeline">{{ showFocus }} 일정 확인하기</a>
+				<a class="btn" @click="showTimeline">일정 확인하기</a>
 				<a class="btn" @click="showTimeline" style="padding: 6px 6px 6px 6px;"><img src="../../assets/refresh/refresh.svg" width=18 height=18></a>
 				<transition name="fade">
-						<div v-if="showTimeTable" min-height="3">
-							<table class="table-condensed table-bordered table-striped">
-								<thead>
-									<tr>
-										<th style="font-size: 12px;" v-for="(item, index) in this.retData" :key="index">{{ this.item.name }}</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr v-for="(i1, index1) in Array(48)" :key="index1">
-										<td v-for="(i2, index2) in Array(7)" :key="index2"></td>
-									</tr>
-								</tbody>
-							</table>
-							<a class="btn" @click="reserve">해당 시각 예약하기</a>
-						</div>
-					</transition>
+					<div v-if="showTimeTable" min-height="3">
+						<table class="table-condensed table-bordered table-striped">
+							<thead>
+								<tr>
+									<th style="font-size: 12px;" v-for="(item, index) in this.retData" :key="index">{{ this.item.name }}</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr v-for="(i1, index1) in Array(48)" :key="index1">
+									<td v-for="(i2, index2) in Array(7)" :key="index2"></td>
+								</tr>
+							</tbody>
+						</table>
+						<select v-model="fromTime">
+							<option disabled :value="-1">시작 시각</option>
+							<option v-for="(t, index) in Array(48)"
+							:key="index"
+							:value="index">
+								{{ parseInt((index) / 24) ? "오후 " : "오전 " }} {{ parseInt(index / 2) % 12 ? parseInt(index / 2) % 12 : 12 }}시 {{ index % 2 ? "30분" : "00분" }}
+							</option>
+						</select>
+						에서
+						<select v-if="fromTime !== -1" v-model="toTime">
+							<option disabled :value="-1">종료 시각</option>
+							<option v-for="(t, index) in Array(48).slice(fromTime, 48)" :key="index" :value="(index + fromTime)">
+								{{ parseInt((index + fromTime) / 24) ? "오후 " : "오전 " }} {{ parseInt((index + fromTime) / 2) % 12 ? parseInt((index + fromTime) / 2) % 12 : 12 }}시 {{ (index + fromTime) % 2 ? "30분" : "00분" }}
+							</option>
+						</select>
+						{{ fromTime !== -1 ? "까지" : "" }}
+						<a class="btn" v-if="toTime !== -1" @click="reserve">해당 시각 예약하기</a>
+					</div>
+				</transition>
 			</center>
 		</div>
 	</div>
@@ -70,19 +80,25 @@ export default {
 								 [[null, true], [null, true], [null, true], [null, true], [null, true], [null, true], [null, true]],
 								 [[null, true], [null, true], [null, true], [null, true], [null, true], [null, true], [null, true]],],
 			focus: new Date(),
-			showTimeTable: false,
+			showTimeTable: false, // true for debugging
 			retData: [],
 			TimeTable: [],
+			fromTime: -1,
+			toTime: -1,
 		}
 	},
 	created () {
-		if (typeof(this.date) === 'string') {
-			if (this.date.length == 8) this.focus = new Date(this.date.slice(0,4), this.date.slice(4,6)-1, this.date.slice(6,8), new Date().getHours())
-		}
+		if (typeof(this.date) === 'string') this.focus = new Date(this.date.slice(0, 4),
+																															this.date.slice(4, 6) - 1,
+																															this.date.slice(6, 8),
+																															new Date().getHours())
 		this.drawCalendar
 	},
 	methods: {
-		changeToday: function () { this.focus = new Date() },
+		changeToday: function () {
+			this.focus = new Date()
+			this.drawCalendar
+		},
 		changeFocus: function (date) {
 			let res = new Date(this.focus.getFullYear(), this.focus.getMonth(), date[0], this.focus.getHours())
 			this.focus = res
@@ -102,24 +118,14 @@ export default {
 		},
 		retTimeList: function () {
 			let xhr = new XMLHttpRequest()
-			xhr.open('GET', '/api/item/'+this.focus.toJSON().slice(0,10).replace(/-/g,""))
+			xhr.open('GET', '/api/item/' + this.focus.toJSON().slice(0, 10).replace(/-/g, ""))
 			xhr.setRequestHeader("Content-type", "application/json")
 			xhr.onreadystatechange = function () {
-				let result = JSON.parse(xhr.responseText)
-				
-				if (result.hasOwnProperty('data')) {
-					this.retData = result.data
-					this.showTimeTable = true
-					/*
-					for (data of result['data']) {
-						this.retData.push(data)
-					}
-					*/
-				}
+				this.showTimeTable = JSON.parse(xhr.responseText).hasOwnProperty('data')
+				if (JSON.parse(xhr.responseText).hasOwnProperty('data')) this.retData = JSON.parse(xhr.responseText).data
 				else {
-					alert('조회에 실패하였습니다.')
 					this.retData = []
-					this.showTimeTable = false
+					alert('조회에 실패하였습니다.')
 				}
 			}
 			xhr.send('{"_csrf": "' + document.cookie.split("_csrf=")[1] + '"}')
@@ -130,50 +136,38 @@ export default {
 			xhr.setRequestHeader("Content-type", "application/json")
 			xhr.onreadystatechange = function () {
 				let result = JSON.parse(xhr.responseText)
-				if (result.hasOwnProperty('success')) alert('예약되었습니다.')
-				else {
-					switch (result.code) {
-						case 0:
-						alert('잘못된 시간값을 입력하셨습니다.')
-						break
-						case 1:
-						alert('이미 예약한 시간대입니다.')
-						break
-						case 2:
-						alert('다른 사람이 예약한 시간대입니다.\n다른 시간대를 예약해주세요.')
-						break
-						default:
-						alert('알 수 없는 오류입니다.\n관리자에게 문의해 주세요.')
-					}
-				}
+				if (result.success) alert('예약되었습니다.')
+				else if (result.code === 0) alert('잘못된 시간값을 입력하셨습니다.')
+				else if (result.code === 1) alert('해당 시간대를 이미 예약하셨습니다.')
+				else if (result.code === 2) alert('다른 사람이 예약한 시간대입니다.\n다른 시간대를 예약해주세요.')
+				else alert('알 수 없는 오류입니다.\n관리자에게 문의해 주세요.')
 				this.showTimeline
 			}
-			xhr.send('{"start":"'+ this.start +
-							 '","end":"' + this.end +
-							 '","itemId":"' + this.user[0] + // TODO: itemID에 들어갈 값
-							 '","date":"' + this.focus +
-							 '","people":"' + this.user[0] + // TODO: people에 들어갈 값
-							 '","_csrf": "' + document.cookie.split("_csrf=")[1] + '"}')
+			xhr.send('{"start": '+ this.fromTime +
+							 ', "end": ' + this.toTime +
+							 ', "itemId": ' + 0 + // TODO: itemID에 들어갈 값
+							 ', "date": ' + this.showFocus.replace(/-/g, '') + 
+							 ', "people": "' + 0 + // TODO: people에 들어갈 값
+							 '", "_csrf": "' + document.cookie.split("_csrf=")[1] + '"}')
 		},
 		showTimeline: function () {
 			this.retTimeList
 			for (item of this.retData) {
-
+				// TODO: 받아온 데이터(retData)를 TimeTable로 변환하기
 			}
+		},
+		drawCalendar: function () {
+			let prev = new Date(this.focus.getFullYear(), this.focus.getMonth(), 0).getDate()
+			let first = new Date(this.focus.getFullYear(), this.focus.getMonth(), 1).getDay()
+			let last = new Date(this.focus.getFullYear(), this.focus.getMonth() + 1, 0).getDate()
+			let i, count = 1
+			for (i = 0; i < first; i++) this.calendar[0][i] = [1 + prev - first + i, false]
+			for (i = first; i < last + first; i++) this.calendar[parseInt(i / 7)][i % 7] = [i + 1 - first, true]
+			for (i = last + first; i < 42; i++) this.calendar[parseInt(i / 7)][i % 7] = [i - last - first + 1, false]
 		}
 	},
 	computed: {
-		showFocus: function () { return this.focus.toJSON().slice(0, 10) },
-		drawCalendar: function () {
-			let prev = new Date(this.focus.getFullYear(), this.focus.getMonth(), 0)
-			let first = new Date(this.focus.getFullYear(), this.focus.getMonth(), 1)
-			let last = new Date(this.focus.getFullYear(), this.focus.getMonth() + 1, 0)
-			let i, count = 1, offset = first.getDay()
-			for (i = 0; i < offset; i++) this.calendar[0][i] = [1 + prev.getDate() - offset + i, false]
-			for (i = offset; i < last.getDate() + offset; i++) this.calendar[parseInt(i / 7)][i % 7] = [i + 1 - offset, true]
-			for (i = last.getDate() + offset; i < 42; i++) this.calendar[parseInt(i / 7)][i % 7] = [i - last.getDate() - offset + 1, false]
-		},
-
+		showFocus: function () { return this.focus.toJSON().slice(0, 10) }
 	}
 }
 </script>
