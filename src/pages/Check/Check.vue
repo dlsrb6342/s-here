8d396abd022eb83945939dced7b17dbe2eee6006
Vaelot <minusgodkeanu@gmail.com>
2017-08-25 10:16:52 +0900

    check 알고리즘 작성(미완)

--- src/pages/Check/Check.vue
+++ src/pages/Check/Check.vue
@@ -1,90 +1,80 @@
 <template>
-  <div class="check view text-center">
-    <v-container>
-        <h2 class="text-center font-exo">Reservation</h2>
-      <v-container>
-        <v-layout row-md column>
-            <v-flex md4 sm12>
-              <v-date-picker
-                v-model="focus"
-                locale="ko-KR"
-              ></v-date-picker>
-            </v-flex>
-            <v-flex md8 sm12 v-if="showTimeTable">
-              <v-layout row-md column>
-                <v-flex md6 class="SFtable">
-                  <v-layout row child-flex class="pa-0 my-1">
-                    <v-card v-for="(item, j) in productId" :key="j" class="pa-1 my-1 mx-0">
-                      <v-card-text class="pa-0">{{ item }}</v-card-text>
-                    </v-card>
-                  </v-layout>
-                  <v-layout row child-flex class="pa-0 my-0"
-                    v-for="i in 48" :key="i">
-                    <v-card v-for="j in productId.length" :key="j" class="pa-1 mx-0 ">
-                      <v-card-text class="caption pa-0"> </v-card-text>
-                    </v-card>
-                  </v-layout>
-                </v-flex>
-                <v-flex md6>
-                  <v-layout column>
-                    <v-flex>
-                      <v-time-picker landscape v-model="fromTime"
-                                     :scrollable="true" :allowed-minutes="grid">
-                      </v-time-picker>
-                    </v-flex>
-                    <v-flex>
-                      <v-time-picker landscape v-model="toTime"
-                                     :scrollable="true" :allowed-minutes="grid">
-                      </v-time-picker>
-                    </v-flex>
-                    <v-flex>
-                      <v-btn lg flat @click="reserve">Submit</v-btn>
-                    </v-flex>
-                  </v-layout>
-                </v-flex>
-              </v-layout>
-            </v-flex>
-          </v-layout>
-      </v-container>
-    </v-container>
-  </div>
+	<div class="check view text-center">
+		<h2 class="text-center font-exo">Reservation</h2>
+		<v-container hidden-sm-and-up>
+			<!-- 휴대폰용 UI 부분-->
+		</v-container>
+		<v-container hidden-xs-only hidden-md-and-up>
+			<!-- 패블릿용 UI 부분 -->
+		</v-container>
+		<v-container>
+			<!-- 데스크톱용 UI -->
+			<v-layout row>
+				<v-flex xs4>
+					<v-date-picker v-model="focus" locale="ko-KR" @click.native="detectFocus"></v-date-picker>
+					{{ this.focus }} 
+					<v-btn @click.native="showTimeTable = !showTimeTable">???</v-btn>
+				</v-flex>
+				<transition name="slide-fade">
+					<v-flex xs8 v-show="showTimeTable">
+						<v-layout row child-flex class="pa-0 my-1">
+							<v-card v-for="(item, j) in productId" :key="j" class="pa-1 my-1 mx-0">
+								<v-card-text class="pa-0">{{ item }}</v-card-text>
+							</v-card>
+						</v-layout>
+						<v-layout row child-flex class="pa-0 my-0" v-for="i in 48" :key="i">
+							<v-card v-for="j in productId.length" :key="j" class="pa-1 mx-0 ">
+								<v-card-text @mousedown="setFromTime(i,j)" @mouseup="this.toTime = i" @touchend="touchDetect(i, j)" class="pa-2"></v-card-text>
+							</v-card>
+						</v-layout>
+					</v-flex>
+				</transition>
+				<v-dialog>
+					
+				</v-dialog>
+			</v-layout>
+		</v-container>
+	</div>
 </template>
 
 <script>
 export default {
 	name: 'check',
 	props: ['date', 'user'],
-	data () {
+	data() {
 		return {
 			focus: null,
-			showTimeTable: true,
+			showTimeTable: false,
 			dialog: true,
 			retData: [],
 			TimeTable: [],
+			selectItem: null,
 			fromTime: null,
 			toTime: null,
-			productId: ["프린터 1", "프린터 2", "프린터 3", "프린터 4", "프린터 5"],
+			touching: false,
+			productId: ["P1", "P2", "P3", "P4", "P5", "T1", "T2", "T3", "T4", "D1", "D2", "D3", "D4", "D5", "D6", "D1", "D2", "D3", "D4", "D5", "D6"],
 		}
 	},
-	created () {
-		if (typeof(this.date) === 'string') {
-		  this.focus = new Date(this.date.slice(0, 4),
-														this.date.slice(4, 6) - 1,
-														this.date.slice(6, 8),
-														new Date().getHours())
+	created() {
+		if (typeof (this.date) === 'string') {
+			this.focus = new Date(this.date.slice(0, 4),
+				this.date.slice(4, 6) - 1,
+				this.date.slice(6, 8),
+				new Date().getHours())
 			this.retTimeList
 		}
 	},
 	methods: {
-		retTimeList: function () {
+    goPage: function (goMessage) { this.$router.push(goMessage) },
+		retTimeList: function() {
+			this.showTimeTable = true
 			let xhr = new XMLHttpRequest()
 			xhr.open('GET', '/api/item/' + this.focus.toJSON().slice(0, 10).replace(/-/g, ""))
 			xhr.setRequestHeader("Content-type", "application/json")
-			xhr.onreadystatechange = function () {
+			xhr.onreadystatechange = function() {
 				let result = JSON.parse(xhr.responseText)
 				if (result.hasOwnProperty('data')) {
 					this.retData = result.data
-					this.showTimeTable = true
 				} else {
 					this.retData = []
 					alert('조회에 실패하였습니다.')
@@ -92,11 +82,11 @@ export default {
 			}
 			xhr.send('{"_csrf": "' + document.cookie.split("_csrf=")[1] + '"}')
 		},
-		reserve: function () {
+		reserve: function() {
 			let xhr = new XMLHttpRequest()
 			xhr.open('POST', '/api/reserve/')
 			xhr.setRequestHeader("Content-type", "application/json")
-			xhr.onreadystatechange = function () {
+			xhr.onreadystatechange = function() {
 				let result = JSON.parse(xhr.responseText)
 				if (result.success) alert('예약되었습니다.')
 				else if (result.code === 0) alert('잘못된 시간값을 입력하셨습니다.')
@@ -105,23 +95,34 @@ export default {
 				else alert('알 수 없는 오류입니다.\n관리자에게 문의해 주세요.')
 				this.showTimeline
 			}
-			xhr.send('{"start": '+ this.fromTime +
-							 ', "end": ' + this.toTime +
-							 ', "itemId": ' + 0 + // TODO: itemID에 들어갈 값
-							 ', "date": ' + this.showFocus.replace(/-/g, '') +
-							 ', "people": "' + 0 + // TODO: people에 들어갈 값
-							 '", "_csrf": "' + document.cookie.split("_csrf=")[1] + '"}')
+			xhr.send('{"start": ' + this.fromTime +
+				', "end": ' + this.toTime +
+				', "itemId": ' + 0 + // TODO: itemID에 들어갈 값
+				', "date": ' + this.showFocus.replace(/-/g, '') +
+				', "people": "' + 0 + // TODO: people에 들어갈 값
+				'", "_csrf": "' + document.cookie.split("_csrf=")[1] + '"}')
 		},
-		showTimeline: function () {
+		showTimeline: function() {
 			this.retTimeList
 			for (item of this.retData) {
 				// TODO: 받아온 데이터(retData)를 TimeTable로 변환하기
 			}
 		},
-		changeFocus: function () {
-			if (this.focus === null) {
-				this.focus = this.dp
+		detectFocus: function() {
+			if (this.focus !== null) this.retTimeList
+		},
+		setFromTime: function (i, j) {
+			this.selectItem = j
+			this.fromTime = i
+		},
+		touchDetect: function (i, j) {
+			if (this.touching) {
+				this.toTime = i
+			} else {
+				this.selectItem = j
+				this.fromTime = i
 			}
+			this.touching = !this.touching
 		}
 	},
 	computed: {
@@ -130,8 +131,24 @@ export default {
 </script>
 
 <style scoped>
-  div .picker{
-    margin: 0 auto;
+div .picker {
+	margin: 0 auto;
+}
 
-  }
+.slide-fade-enter-active {
+	transition: all .2s ease;
+}
+
+.slide-fade-leave-active {
+	transition: all .2s cubic-bezier(.1, .9, .1, .9);
+}
+
+.slide-fade-enter,
+.slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */
+
+{
+	transform: translateX(20px);
+	opacity: 0;
+}
 </style>
