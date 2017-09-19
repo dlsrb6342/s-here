ae64fa019eb055da5aae4550efa42a2df56ae452
dlsrb6342 <dlsrb6342@gmail.com>
2017-09-19 13:52:48 +0900

    Edit item query / Add admin timetable api

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,22 +1,58 @@
 import express from 'express';
-import { Trouble } from '../models';
+import { Trouble, Reservation } from '../models';
 
 const router = express.Router();
 
-router.use((req, res, next) => {
-  if (typeof req.session.userInfo.name !== "admin") {
-    return res.status(403).json({
-      error: "NOT_ALLOWED",
-      code: 0
-    })
-  };
-  next();
-});
+//router.use((req, res, next) => {
+//  if (typeof req.session.userInfo.name !== "admin") {
+//    return res.status(403).json({
+//      error: "NOT_ALLOWED",
+//      code: 0
+//    })
+//  };
+//  next();
+//});
 
 router.get('/trouble', (req, res) => {
   Trouble.find({}).sort('-time').exec((err, troubles) => {
     if (err) throw err;
     return res.json({ data: troubles });
+  });
+});
+
+router.get('/timetable/:date', (req, res) => {
+  let date = parseInt(req.params.date);
+  let today = new Date();
+  if (date < today.toJSON().slice(0,10).split('-').join('')) {
+    return res.status(400).json({
+      error: "INVALID_DATE",
+      code: 0
+    });
+  }
+  Reservation.find({ $and: [
+    { start: { $gte: date * 100 }}, { end: { $lt: (date + 1) * 100 }}]}).
+    populate([
+      { path: 'user', select: 'student_id name' },
+      { path: 'item', select: 'name'}]).
+    sort({'item': 1, 'start': 1}).exec((err, reservations) => {
+    if (err) throw err;
+    let reservationMap = {};
+    reservations.forEach(reservation => {
+      if (reservationMap.hasOwnProperty(reservation.item.name)) {
+        reservationMap[reservation.item.name].push({
+          user: reservation.user, 
+          start: reservation.start, 
+          end: reservation.end
+        });
+      } else {
+        reservationMap[reservation.item.name] = [{
+          user: reservation.user, 
+          start: reservation.start, 
+          end: reservation.end
+        }];
+      }
+    });
+    return res.json({ data: reservationMap });
   });
 });
 
--- server/routes/item.js
+++ server/routes/item.js
@@ -14,7 +14,7 @@ router.use((req, res, next) => {
 });
 
 router.get('/:date', (req, res) => {
-  let date = req.params.date;
+  let date = parseInt(req.params.date);
   let today = new Date();
   if (date < today.toJSON().slice(0,10).split('-').join('')) {
     return res.status(400).json({
@@ -22,21 +22,22 @@ router.get('/:date', (req, res) => {
       code: 0
     });
   }
-  Item.find({ active: true }, (err, items) => {
+  Item.aggregate([
+    { $match: { 
+      active: true 
+    }},
+    { $unwind: '$occupied' },
+    { $match: { $and: [
+      { occupied: { $gte: date * 100 }}, 
+      { occupied: { $lt: (date + 1) * 100 }}
+    ]}},
+    { $group : {
+      _id: "$_id",
+      occupied: { $addToSet : "$occupied" }
+    }}
+  ], (err, items) => {
     if (err) throw err;
-    let itemList = [], itemMap = {};
-    items.forEach(item => {
-      if (item.active) {
-        itemMap = {};
-        itemMap['name'] = item.name;
-        itemMap['_id'] = item._id;
-        itemMap['occupied'] = item.occupied.filter(value => {
-          return (value >= date * 100 && value < (date +1) * 100);
-        });
-        itemList.push(itemMap);
-      };
-    });
-    return res.json({ data: itemList });
+    return res.json({ data: items });
   });
 });
 
